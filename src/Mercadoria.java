0ce972d44d6edbbc9f30d5ce7293dd1d6cd695b8
Nicolas <nicolas.docolas1804@gmail.com>
2024-07-11 11:59:52 -0300

    implementacao em record

--- src/Mercadoria.java
+++ src/Mercadoria.java
@@ -1,24 +1,2 @@
 
-public class Mercadoria {
-    private String codigo;
-    private String descricao;
-    private double preco;
-
-    public Mercadoria(String codigo, String descricao, double preco) {
-        this.codigo = codigo;
-        this.descricao = descricao;
-        this.preco = preco;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-}
+public record Mercadoria(String codigo, String descricao, double preco) {}
